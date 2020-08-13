75e24379319f346bff8cec27fbe540bd766e4df9
Omid Taheri <omid.taheri.71@gmail.com>
2020-08-13 14:58:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='bps_torch',
+    include_package_data=True,
+    #packages=find_packages(),
+    description='A Pytorch Inplementation of bps 3D representation',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    version='0.1',
+    url='https://github.com/otaheri/MANO',
+    author='Omid Taheri',
+    author_email='omid.taheri@tuebingen.mpg.de',
+    maintainer='Omid Taheri',
+    maintainer_email='omid.taheri@tuebingen.mpg.de',
+    #keywords=['pip','MANO'],
+    install_requires=[
+          'numpy>=1.16.2',
+          'torch>=1.0.1.post2',
+          'torchgeometry>=0.1.2'
+      ],
+    packages=['bps_torch']
+      
+    )
